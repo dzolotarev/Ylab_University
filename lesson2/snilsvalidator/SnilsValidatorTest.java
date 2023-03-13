d689c0fc8da127071a17f2c41c596c50aa2a325a
Denis Zolotarev <judaeus@gmail.com>
2023-03-13 16:10:13 +0200

    clean master

--- lesson2/snilsvalidator/SnilsValidatorTest.java
+++
@@ -1,8 +0,0 @@
-package lesson2.snilsvalidator;
-
-public class SnilsValidatorTest {
-    public static void main(String[] args) {
-        System.out.println(new SnilsValidatorImpl().validate("01468870570")); //false
-        System.out.println(new SnilsValidatorImpl().validate("90114404441")); //true
-    }
-}
